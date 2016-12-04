7eb27a81cbae4cf2e17387db0d148615409dda49
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-05 03:06:44 +0700

    PlayerThread and PlayerImpl classes merged

--- src/main/java/org/luwrain/player/PlayerImpl.java
+++ src/main/java/org/luwrain/player/PlayerImpl.java
@@ -17,63 +17,223 @@
 package org.luwrain.player;
 
 import java.util.*;
+import java.net.*;
+import java.nio.file.*;
 
 import org.luwrain.core.*;
-//import org.luwrain.util.RegistryPath;
+import org.luwrain.player.backends.*;
 
-//import org.luwrain.player.backends.*;
-
-class PlayerImpl implements Player
+class PlayerImpl implements Player, org.luwrain.player.backends.Listener
 {
-    private final PlayerThread thread = new PlayerThread();
-    private Registry registry;
+    private final Registry registry;
+    private final Vector<Listener> listeners = new Vector<Listener>();
 
-PlayerImpl(Registry registry)
+    private Playlist currentPlaylist = null;
+    private BackEnd currentPlayer = null;
+    private int currentTrackNum = -1;
+    private long currentPos = 0;
+
+    PlayerImpl(Registry registry)
     {
 	NullCheck.notNull(registry, "registry");
 	this.registry = registry;
     }
 
-    @Override public void play(Playlist playlist,
-			       int startingTrackNum, long startingPosMsec)
+
+
+    @Override public synchronized void play(Playlist playlist,
+			   int startingTrackNum, long startingPosMsec)
     {
 	NullCheck.notNull(playlist, "playlist");
-thread.play(playlist, startingTrackNum, startingPosMsec);
+	if (playlist.getPlaylistItems() == null || playlist.getPlaylistItems().length < 1)
+	    return;
+	if (currentPlaylist != null)
+	    stop();
+	currentPlaylist = playlist;
+	currentTrackNum = startingTrackNum;
+	currentPos = startingPosMsec;
+	final Task task = createTask();
+	if (task == null)
+	    return;
+	Log.debug("player", "starting pos is " + currentPos);
+	task.setStartPosMsec(currentPos);
+	currentPlayer = BackEnd.createBackEnd(this, "jlayer");
+	//currentPlayer = BackEnd.createBackEnd(this, "SoundPlayer");
+	//	currentPlayer = BackEnd.createBackEnd(this, "OggPlayer");
+	for(Listener l: listeners)
+	{
+	    l.onNewPlaylist(playlist);
+	    l.onNewTrack(playlist, currentTrackNum);
+	}
+	currentPlayer.play(task);
     }
 
-    @Override public void stop()
+    @Override public synchronized void stop()
     {
-	thread.stop();
+	if (currentPlayer == null)
+	    return;
+	currentPlayer.stop();
+	notifyListeners((listener)->listener.onPlayerStop());
+	currentPlayer = null;
+	currentPlaylist = null;
     }
 
-    @Override public void pauseResume()
+    @Override public synchronized void pauseResume()
     {
-	thread.pauseResume();
+	if (currentPlayer != null)
+	{
+	    //pausing
+	    currentPlayer.stop();
+	    currentPlayer = null;
+	    return;
+	}
+	if (currentPlaylist == null)
+	    return;
+	//resuming
+	final Task task = createTask();
+	if (task == null)
+	    return;
+	task.setStartPosMsec(currentPos);
+	notifyListeners((listener)->listener.onTrackTime(currentPlaylist, currentTrackNum, currentPos));
+	currentPlayer = BackEnd.createBackEnd(this, "jlayer");
+	currentPlayer.play(task);
     }
 
-    @Override public void jump(long offsetMsec)
+    @Override public synchronized void jump(long offsetMsec)
     {
-thread.jump(offsetMsec);
+	if (currentPlaylist == null || currentPlaylist.isStreaming())
+	    return;
+	if (currentPlayer != null)
+	    {
+			currentPlayer.stop();
+	currentPlayer = null;
+	    }
+	final Task task = createTask();
+	if (task == null)
+	    return;
+	currentPos += offsetMsec;
+	if (currentPos < 0)
+	    currentPos = 0;
+	task.setStartPosMsec(currentPos);
+	notifyListeners((listener)->listener.onTrackTime(currentPlaylist, currentTrackNum, currentPos));
+		currentPlayer = BackEnd.createBackEnd(this, "jlayer");
+	currentPlayer.play(task); 
     }
 
-    @Override public void nextTrack()
+	@Override public synchronized void nextTrack()
+			  {
+	if (currentPlaylist == null || currentPlaylist.isStreaming())
+	    return;
+	if (currentTrackNum + 1 >= currentPlaylist.getPlaylistItems().length)
+	    return;
+	++currentTrackNum;
+	if (currentPlayer != null)
+	currentPlayer.stop();
+	final Task task = createTask();
+	if (task == null)
+	    return;
+	currentPlayer = BackEnd.createBackEnd(this, "jlayer");
+	currentPos = 0;
+	notifyListeners((listener)->listener.onNewTrack(currentPlaylist, currentTrackNum));
+	notifyListeners((listener)->listener.onTrackTime(currentPlaylist, currentTrackNum, 0));
+	currentPlayer.play(task); 
+			  }
+
+    @Override public synchronized void prevTrack()
     {
-	thread.nextTrack();
+	if (currentPlaylist == null || currentPlaylist.isStreaming())
+	    return;
+	if (currentTrackNum <= 0)
+	    return;
+	--currentTrackNum;
+	if (currentPlayer != null)
+	currentPlayer.stop();
+	final Task task = createTask();
+	if (task == null)
+	    return;
+	currentPlayer = BackEnd.createBackEnd(this, "jlayer");
+	currentPos = 0;
+	notifyListeners((listener)->listener.onNewTrack(currentPlaylist, currentTrackNum));
+	notifyListeners((listener)->listener.onTrackTime(currentPlaylist, currentTrackNum, 0));
+	currentPlayer.play(task); 
     }
 
-    @Override public void prevTrack()
+    @Override public synchronized Playlist getCurrentPlaylist()
     {
-	thread.prevTrack();
+	return currentPlaylist;
     }
 
-    @Override public Playlist getCurrentPlaylist()
+    @Override public synchronized int getCurrentTrackNum()
     {
-	return thread.getCurrentPlaylist();
+	return currentTrackNum;
+    } 
+
+    @Override public synchronized void onPlayerBackEndTime(long msec)
+    {
+	if (currentPlaylist == null || currentPlayer == null)
+	    return;
+	if (currentPos <= msec && msec < currentPos + 50)
+	    return;
+	currentPos = msec;
+	notifyListeners((listener)->listener.onTrackTime(currentPlaylist, currentTrackNum, currentPos));
+	currentPlaylist.updateStartingPos(currentTrackNum, currentPos);
     }
 
-    @Override public int getCurrentTrackNum()
+    @Override public synchronized void onPlayerBackEndFinish()
     {
-	return thread.getCurrentTrackNum();
+	if (currentPlaylist == null)
+	    return;
+    }
+
+    @Override public synchronized void addListener(Listener listener)
+    {
+	NullCheck.notNull(listener, "listener");
+	for(Listener l: listeners)
+	    if (l == listener)
+		return;
+	listeners.add(listener);
+    }
+
+    @Override public synchronized void removeListener(Listener listener)
+    {
+	NullCheck.notNull(listener, "listener");
+	for(int i = 0;i < listeners.size();++i)
+	    if (listeners.get(i) == listener)
+	    {
+		listeners.remove(i);
+		break;
+	    }
+    }
+
+    private void notifyListeners(ListenerNotification notification)
+    {
+	for(Listener l: listeners)
+	    notification.notify(l);
+    }
+
+    private Task createTask()
+    {
+	    final String url = currentPlaylist.getPlaylistItems()[currentTrackNum];
+	try {
+	    Log.debug("player", "creating task for " + url);
+	    Task task = new Task(new URL(url));
+	    if (task.url().getProtocol().equals("file"))
+	    {
+		task = new Task(Paths.get(task.url().getFile()));
+	    }
+	    return task;
+	}
+	catch (Exception e)
+	{
+	    Log.error("player", "unable to create a task for " + url + ":" + e.getClass().getName() + ":" + e.getMessage());
+	    e.printStackTrace();
+	    return null;
+	}
+    }
+
+    private interface ListenerNotification
+    {
+	void notify(Listener listener);
     }
 
     @Override public Playlist[] loadRegistryPlaylists()
@@ -91,15 +251,5 @@ thread.jump(offsetMsec);
 	return res.toArray(new Playlist[res.size()]);
     }
 
-    @Override public void addListener(Listener listener)
-    {
-	NullCheck.notNull(listener, "listener");
-thread.addListener(listener);
-    }
 
-    @Override public void removeListener(Listener listener)
-    {
-	NullCheck.notNull(listener, "listener");
-	thread.removeListener(listener);
-    }
 }
