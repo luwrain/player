079114f79de87640e30f7881f060e082d588e9f0
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-08-20 02:39:48 +0700

    Proper new playlist conversation

--- src/main/java/org/luwrain/app/player/Conversations.java
+++ src/main/java/org/luwrain/app/player/Conversations.java
@@ -31,39 +31,96 @@ class Conversations
 	this.luwrain = luwrain;
     }
 
-static class NewPlaylistParams
-{
-    enum Type {DIRECTORY, STREAMING, WITH_BOOKMARK};
-
-    final Type type;
-    final String name;
-    final String path;
-
-    NewPlaylistParams(Type type, String name, String path)
+    static class NewPlaylistParams
     {
-	NullCheck.notNull(type, "type");
-	NullCheck.notEmpty(name, "name");
-	this.type = type;
-	this.name = name;
-	this.path = path;
+	enum Type {DIRECTORY, M3U, STREAMING, WITH_BOOKMARK_DIR, WITH_BOOKMARK_M3U};
+
+	final Type type;
+	final String name;
+	final String arg;
+
+	NewPlaylistParams(Type type, String name, String arg)
+	{
+	    NullCheck.notNull(type, "type");
+	    NullCheck.notEmpty(name, "name");
+	    NullCheck.notNull(arg, "arg");
+	    this.type = type;
+	    this.name = name;
+	    this.arg = arg;
+	}
     }
-}
 
     NewPlaylistParams addPlaylist()
     {
-	final String directory = "Каталог с музыкальными файлами";
-	final String withBookmark = "Говорящая книга";
-	final String streaming = "Интернет-радио";
-	final Object type = Popups.fixedList(luwrain, "Тип нового плейлиста:", new Object[]{directory, withBookmark, streaming});
-	if (type == null)
+	final String directory = "Каталог с музыкальными файлами";//FIXME:
+	final String m3u = "M3U-плейлист";//FIXME:
+	final String withBookmarkDir = "Говорящая книга";//FIXME:
+	final String withBookmarkM3u = "Говорящая книга с M3U-плейлистом";//FIXME:
+	final String streaming = "Интернет-радио";//FIXME:
+	final Object typeRes = Popups.fixedList(luwrain, "Тип нового плейлиста:", new Object[]{directory, m3u, withBookmarkDir, withBookmarkM3u, streaming});//FIXME:
+	if (typeRes == null)
 	    return null;
-	final String path;
-	if (type == directory)
+	final NewPlaylistParams.Type type;
+	final String arg;
+	if (typeRes == directory)
 	{
-	    final File file = Popups.directory(luwrain, "Добавление плейлиста", "Укажите каталог с музыкальными записями:", null);
+	    final File file = Popups.directory(luwrain, "Добавление плейлиста", "Каталог с музыкальными записями:", null);
 	    if (file == null)
 		return null;
-	}
-	return null;
-}
+	    type = NewPlaylistParams.Type.DIRECTORY;
+	    arg = file.getAbsolutePath();
+	} else
+	    if (typeRes == m3u)
+	    {
+		final File file = Popups.path(luwrain, "Добавление плейлиста", "Путь к M3U-файлу:", (fileToCheck, announce)->{
+			if (fileToCheck.isDirectory())
+			{
+			    if (announce)
+				luwrain.message("" + fileToCheck + " является каталогом", Luwrain.MessageType.ERROR);//FIXME:
+			    return false;
+			}
+			return true;
+		    });
+		if (file == null)
+		    return null;
+		type = NewPlaylistParams.Type.M3U;
+		arg = file.getAbsolutePath();
+	    } else
+		if (typeRes == withBookmarkDir)
+		{
+		    final File file = Popups.directory(luwrain, "Добавление плейлиста", "Каталог с говорящей книгой:", null);
+		    if (file == null)
+			return null;
+		    type = NewPlaylistParams.Type.WITH_BOOKMARK_DIR;
+		    arg = file.getAbsolutePath();
+		} else
+		    if (typeRes == withBookmarkM3u)
+		    {
+			final File file = Popups.path(luwrain, "Добавление плейлиста", "Путь к M3U-файлу говорящей книги:", (fileToCheck, announce)->{
+				if (fileToCheck.isDirectory())
+				{
+				    if (announce)
+					luwrain.message("" + fileToCheck + " является каталогом", Luwrain.MessageType.ERROR);//FIXME:
+				    return false;
+				}
+				return true;
+			    });
+			if (file == null)
+			    return null;
+			type = NewPlaylistParams.Type.WITH_BOOKMARK_M3U;
+			arg = file.getAbsolutePath();
+		    } else
+			if (typeRes == streaming)
+			{
+			    arg = Popups.simple(luwrain, "Добавление интернет-радио", "URL потокового вещания интернет-радио:", "http://");
+			    if (arg == null || arg.trim().isEmpty())
+				return null;
+			    type = NewPlaylistParams.Type.STREAMING;
+			} else
+			    return null;
+	final String title = Popups.simple(luwrain, "Добавление нового плейлиста", "Название нового плейлиста:", "");
+	if (title == null || title.trim().isEmpty())
+	    return null;
+	return new NewPlaylistParams(type, title, arg);
+    }
 }
